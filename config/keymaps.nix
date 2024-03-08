2c159834da311fb789dc05d1ecb5449ce75788e6
Lukas Wurzinger <lukas@wrz.one>
2024-03-08 23:20:25 +0100

    bindingsg

--- config/keymaps.nix
+++ config/keymaps.nix
@@ -1,88 +1,48 @@
-{
-  keymaps = [
-    # Fast split switching
-    {
-      mode = "n";
-      key = "<C-j>";
-      action = "<C-w><C-j>";
-    }
-    {
-      mode = "n";
-      key = "<C-k>";
-      action = "<C-w><C-k>";
-    }
-    {
-      mode = "n";
-      key = "<C-l>";
-      action = "<C-w><C-l>";
-    }
-    {
-      mode = "n";
-      key = "<C-h>";
-      action = "<C-w><C-h>";
-    }
-
-    # Keep cursor position when joining lines
-    {
-      mode = "n";
-      key = "J";
-      action = "mzJ`z";
-    }
-
-    # Center the cursor when moving up or down the document
-    {
-      mode = "n";
-      key = "<C-d>";
-      action = "<C-d>zz";
-    }
-    {
-      mode = "n";
-      key = "<C-u>";
-      action = "<C-u>zz";
-    }
-
-    # Center the cursor when searching
-    {
-      mode = "n";
-      key = "n";
-      action = "nzzzv";
-    }
-    {
-      mode = "n";
-      key = "N";
-      action = "Nzzzv";
-    }
-
+{lib, ...}: {
+  keymaps =
+    [
+      # Keep cursor position when joining lines
+      {
+        mode = "n";
+        key = "J";
+        action = "mzJ`z";
+      }
+    ]
+    ++
+    # Fast split movement
+    (
+      map (
+        k: {
+          mode = "n";
+          key = "<C-${k}>";
+          action = "<C-w><C-${k}>";
+        }
+      ) ["h" "j" "k" "l"]
+    )
+    ++
+    # Fast tab movement
+    (
+      map (
+        n: {
+          mode = "n";
+          key = "<A-${
+            if n == 10
+            then "0"
+            else builtins.toString n
+          }>";
+          action = "${builtins.toString n}gt";
+        }
+      ) (lib.range 1 10)
+    )
+    ++
     # Use system clipboard
-    {
-      mode = ["n" "v"];
-      key = "<Leader>y";
-      action = "\"+y";
-    }
-    {
-      mode = ["n" "v"];
-      key = "<Leader>Y";
-      action = "\"+Y";
-    }
-    {
-      mode = ["n" "v"];
-      key = "<Leader>d";
-      action = "\"+d";
-    }
-    {
-      mode = ["n" "v"];
-      key = "<Leader>D";
-      action = "\"+D";
-    }
-    {
-      mode = ["n" "v"];
-      key = "<Leader>p";
-      action = "\"+p";
-    }
-    {
-      mode = ["n" "v"];
-      key = "<Leader>P";
-      action = "\"+P";
-    }
-  ];
+    (
+      map (
+        k: {
+          mode = ["n" "v"];
+          key = "<Leader>${k}";
+          action = "\"+${k}";
+        }
+      ) ["y" "Y" "d" "D" "p" "P"]
+    );
 }
